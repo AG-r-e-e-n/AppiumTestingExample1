f1038aed7f173ab55cb4bd8e60b4d60237ab504d
unknown <Evergreen1504@gmail.com>
2025-02-10 21:10:17 -0500

    Esta es la primera prueba desordenada

--- src/test/java/Pruebas/AppiumTest.java
+++ src/test/java/Pruebas/AppiumTest.java
@@ -1,9 +1,18 @@
 package Pruebas;
 
-import io.appium.java_client.MobileElement;
+import java.io.File;
+import java.io.IOException;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.commons.io.FileUtils;
+import org.testng.annotations.Test;
+
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
 import io.appium.java_client.remote.MobileCapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.OutputType;
 import java.net.URL;
 
 public class AppiumTest {
@@ -11,17 +20,32 @@ public class AppiumTest {
         // Set the Desired Capabilities
         DesiredCapabilities caps = new DesiredCapabilities();
         caps.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
-        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "Android Emulator"); // Change this to your device name
-        caps.setCapability(MobileCapabilityType.APP, "/path/to/your/app.apk"); // Change this to the path of your app
+        caps.setCapability(MobileCapabilityType.DEVICE_NAME, "R9JR40PS32J"); // Change this to your device name
+        caps.setCapability(MobileCapabilityType.APP, "C:/Users/Abraham Green/Documents/AppiumEjemplo1/General-Store-AppiumTesting/src/General-Store.apk"); // Change this to the path of your app
+        caps.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
 
         try {
             // Initialize the driver
-            AndroidDriver<MobileElement> driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"), caps);
+            AndroidDriver<AndroidElement> driver = new AndroidDriver<AndroidElement>(new URL("http://localhost:4723/"), caps);
+            driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 
-            // Write your test here
+            driver.findElementByClassName("android.widget.Spinner").click();
+    		
+    	    driver.findElementByAndroidUIAutomator("new UiScrollable(new UiSelector()).scrollIntoView(text(\"Panama\"));").click();
+    		
+    		driver.findElementByAndroidUIAutomator("text(\"Enter name here\")").sendKeys("AG");
+    		
+    		driver.hideKeyboard();
+    		
+    		driver.findElementByAndroidUIAutomator("text(\"Male\")").click();
+    		
+
+    		driver.findElementByAndroidUIAutomator("text(\"Let's  Shop\")").click();
+    		
+            
             // Example: Find an element and click it
-            MobileElement element = driver.findElementById("com.example:id/button");
-            element.click();
+            /*AndroidElement element = driver.findElementById("com.example:id/button");
+            element.click();*/
 
             // Quit the driver
             driver.quit();
